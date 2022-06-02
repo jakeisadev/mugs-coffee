9c1b912be43d416e22640275a1b1550501874346
jakeisadev <starowicz@protonmail.com>
2022-06-01 21:43:13 -0400

    added react extensions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './index.css';
-import './home.css'
-import 'bootstrap/dist/css/bootstrap.css'
+import './home.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {useState} from 'react';
 
 function App() {
   return (
